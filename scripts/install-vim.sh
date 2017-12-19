f003f50cc55efdd87fb4ab9158c233f8360c853b
raa0121 <raa0121@gmail.com>
2017-12-19 11:11:21 +0900

    add test and .travis.yml

---
+++ scripts/install-vim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ev
+
+case "${TRAVIS_OS_NAME}" in
+	linux)
+		git clone --depth 1 --branch "${VIM_VERSION}" https://github.com/vim/vim /tmp/vim
+		cd /tmp/vim
+		./configure --prefix="${HOME}/vim" --with-features=huge \
+			--enable-perlinterp --enable-pythoninterp --enable-python3interp \
+			--enable-rubyinterp --enable-luainterp --enable-fail-if-missing
+		make -j2
+		make install
+		;;
+	osx)
+		export HOMEBREW_NO_AUTO_UPDATE=1
+		brew update
+		brew install macvim --with-override-system-vim --with-lua
+		;;
+	*)
+		echo "Unknown value of \${TRAVIS_OS_NAME}: ${TRAVIS_OS_NAME}"
+		exit 65
+		;;
+esac
